5ade0dbbddb560e556293dfa18aea6f17b4470cf
PieMenard <pinedo.m.gustavo@gmail.com>
2025-09-12 22:38:43 -0400

    finish lesson 63

---
+++ 63-post-processing-with-r3f/src/Drunk.jsx
@@ -0,0 +1,9 @@
+import { forwardRef } from 'react';
+import DrunkEffect from './DrunkEffect';
+
+const Drunk = (props, ref) => {
+    const effect = new DrunkEffect(props, ref);
+
+    return <primitive ref={ref} object={effect} />;
+};
+export default forwardRef(Drunk);
